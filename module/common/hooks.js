2768301ed261c36aa598c5fa13a1793338b3de45
khaali-dev <github@jgol.fr>
2022-07-28 20:08:22 +0200

    :sparkles: auto-imported user guide

--- module/common/hooks.js
+++ module/common/hooks.js
@@ -1,5 +1,9 @@
 
 export default function registerHooks() {
+    
+    Hooks.once('ready', () => {
+        showUserGuide();
+      });
 
     Hooks.on('createActor', async (document, options, userId) => {
         if (game.user.isGM) {
@@ -55,4 +59,49 @@ export default function registerHooks() {
         }
     
     });
+
+          
+    async function showUserGuide() {
+        if (game.user.isGM) {
+            const newVer = game.system.version;
+            const userGuideJournalName = 'Présentation du Système Clé en main';
+            const userGuideCompendiumLabel = 'systemuserguidefr';
+      
+            let currentVer = '0';
+            let oldUserGuide = game.journal.getName(userGuideJournalName);
+            if (oldUserGuide !== undefined && oldUserGuide !== null && oldUserGuide.getFlag('cleenmain', 'ver') !== undefined) {
+              currentVer = oldUserGuide.getFlag('cleenmain', 'ver');
+            }
+            if (newVer === currentVer) {
+              // Up to date
+              return;
+            }
+      
+            let newReleasePack = game.packs.find((p) => p.metadata.name === userGuideCompendiumLabel);
+            if (newReleasePack === null || newReleasePack === undefined) {
+              console.log('No conpendium found for the system guide');
+              return;
+            }
+            await newReleasePack.getIndex();
+      
+            let newUserGuide = newReleasePack.index.find((j) => j.name === userGuideJournalName);
+            if (newUserGuide === undefined || newUserGuide === null) {
+                console.log('No system guide found in the conpendium');
+              return;
+            }
+      
+            // Don't delete until we have new release Pack
+            if (oldUserGuide !== null && oldUserGuide !== undefined) {
+              await oldUserGuide.delete();
+            }
+      
+            await game.journal.importFromCompendium(newReleasePack, newUserGuide._id);
+            let newReleaseJournal = game.journal.getName(newUserGuide.name);
+      
+            await newReleaseJournal.setFlag('cleenmain', 'ver', newVer);
+      
+            // Show journal
+            await newReleaseJournal.sheet.render(true, { sheetMode: 'text' });
+        }
+      }
 }
