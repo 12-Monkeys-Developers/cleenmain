950661e25b390b678455624cc61702b888858b26
khaali-dev <github@jgol.fr>
2023-01-25 10:50:08 +0100

    :bug: combat tracker player error message

--- module/combat/combatant.js
+++ module/combat/combatant.js
@@ -2,7 +2,7 @@ export default class CemCombatant extends Combatant {
 
     _onCreate(data, options, userID) {
         super._onCreate(data, options, userID);
-        this.setFlag("world", "hasActed", false);        
+        if(game.user.isGM) this.setFlag("world", "hasActed", false);        
     }
    
 }
